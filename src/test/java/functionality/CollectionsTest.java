b2582c9ed5aac1c75dcafef6299461943cb68aa7
alejes <alejes@yandex.ru>
2016-03-20 13:28:52 +0300

    minimize

--- src/test/java/functionality/CollectionsTest.java
+++ src/test/java/functionality/CollectionsTest.java
@@ -27,40 +27,32 @@ public class CollectionsTest {
     }
     @Test
     public void testMap() throws Exception {
-        Integer input[] = {1,2,3,4,5,6};
-        Integer result[] = {6,7,8,9,10, 11};
-        List<Integer> inputList = Arrays.asList(input);
-        List<Integer> resultList = Arrays.asList(result);
+        List<Integer> inputList = Arrays.asList(1,2,3,4,5,6);
+        List<Integer> resultList = Arrays.asList(6,7,8,9,10, 11);
 
         assertTrue(resultList.equals(Collections.map(new FunPlus5(), inputList)));
     }
 
     @Test
     public void testFilter() throws Exception {
-        Integer input[] = {1,2,3,4,5,6};
-        Integer result[] = {2,4,6};
-        List<Integer> inputList = Arrays.asList(input);
-        List<Integer> resultList = Arrays.asList(result);
+        List<Integer> inputList = Arrays.asList(1,2,3,4,5,6);
+        List<Integer> resultList = Arrays.asList(2,4,6);
 
         assertTrue(resultList.equals(Collections.filter(new EvenNumber(), inputList)));
     }
 
     @Test
     public void testTakeWhile() throws Exception {
-        Integer input[] = {2,4,6,8,10,11,12,14,23,96};
-        Integer result[] = {2,4,6,8,10};
-        List<Integer> inputList = Arrays.asList(input);
-        List<Integer> resultList = Arrays.asList(result);
+        List<Integer> inputList = Arrays.asList(2,4,6,8,10,11,12,14,23,96);
+        List<Integer> resultList = Arrays.asList(2,4,6,8,10);
 
         assertTrue(resultList.equals(Collections.takeWhile(new EvenNumber(), inputList)));
     }
 
     @Test
     public void testTakeUnless() throws Exception {
-        Integer input[] = {1,3,5,7,9,11,24,29,1092,3323};
-        Integer result[] = {1,3,5,7,9,11};
-        List<Integer> inputList = Arrays.asList(input);
-        List<Integer> resultList = Arrays.asList(result);
+        List<Integer> inputList = Arrays.asList(1,3,5,7,9,11,24,29,1092,3323);
+        List<Integer> resultList = Arrays.asList(1,3,5,7,9,11);
 
         assertTrue(resultList.equals(Collections.takeUnless(new EvenNumber(), inputList)));
     }
@@ -88,31 +80,26 @@ public class CollectionsTest {
 
     @Test
     public void testFoldl() throws Exception {
-        Integer input[] = {1,2,3,4,5,6,7,8,9,10};
-        List<Integer> inputList = Arrays.asList(input);
+        List<Integer> inputList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
         assertEquals(55, (int)Collections.foldl(new Sum(), 0, inputList));
 
-        String inputStr[] = {"M", "A", "M", "A"};
-        List<String> inputStrList = Arrays.asList(inputStr);
+        List<String> inputStrList = Arrays.asList("M", "A", "M", "A");
         assertEquals("MAMA", (String)Collections.foldl(new Concat(), "", inputStrList));
 
-        Integer inputSub[] = {1,2,3,4,5};
-        List<Integer> inputSubList = Arrays.asList(inputSub);
+        List<Integer> inputSubList = Arrays.asList(1,2,3,4,5);
 
         assertEquals(85, (int)Collections.foldl(new Sub(), 100, inputSubList));
     }
 
     @Test
     public void testFoldr() throws Exception {
-        Integer input[] = {1,2,3,4,5,6,7,8,9,10};
-        List<Integer> inputList = Arrays.asList(input);
+        List<Integer> inputList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
         assertEquals(55, (int)Collections.foldr(new Sum(), 0, inputList));
 
-        //String inputStr[] = {"M", "A", "M", "A"};
-        //List<String> inputStrList = Arrays.asList(inputStr);
-        //assertEquals("МАМА", (String)Collections.foldr(new Concat(), "", inputStrList));
+        List<String> inputStrList = Arrays.asList("M", "A", "M", "A");
+        assertEquals("MAMA", (String)Collections.foldr(new Concat(), "", inputStrList));
 
         Integer inputSub[] = {1,2,3,4,5};
         List<Integer> inputSubList = Arrays.asList(inputSub);
