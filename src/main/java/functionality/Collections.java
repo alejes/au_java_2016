58366a9ecc1cdbaff16588016adec2481643a925
alejes <alejes@yandex.ru>
2016-03-21 22:35:47 +0300

    mini fix before deadline

--- src/main/java/functionality/Collections.java
+++ src/main/java/functionality/Collections.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-/**
- * Created by Alexey on 18.03.2016.
- */
 public class Collections {
-    public static <A, R> List<R> map(Function1<A, R> f, Iterable<A> it) {
+    public static <A, R> List<R> map(Function1<? super A, R> f, Iterable<A> it) {
         List<R> lst = new ArrayList<>();
         for (A el : it){
             lst.add(f.apply(el));
@@ -16,7 +13,7 @@ public class Collections {
         return lst;
     }
 
-    public static <A> List<A> filter(Predicate<A> p, Iterable<A> it) {
+    public static <A> List<A> filter(Predicate<? super A> p, Iterable<A> it) {
         List<A> lst = new ArrayList<>();
         for (A el : it){
             if (p.apply(el)) {
@@ -26,7 +23,7 @@ public class Collections {
         return lst;
     }
 
-    public static <A> List<A> takeWhile(Predicate<A> p, Iterable<A> it) {
+    public static <A> List<A> takeWhile(Predicate<? super A> p, Iterable<A> it) {
         List<A> lst = new ArrayList<>();
         for (A el : it){
             if (!p.apply(el)) {
@@ -37,7 +34,7 @@ public class Collections {
         return lst;
     }
 
-    public static <A> List<A> takeUnless(Predicate<A> p, Iterable<A> it) {
+    public static <A> List<A> takeUnless(Predicate<? super A> p, Iterable<A> it) {
         List<A> lst = new ArrayList<>();
         for (A el : it){
             if (p.apply(el)) {
@@ -48,14 +45,14 @@ public class Collections {
         return lst;
     }
 
-    public static <A, R> R foldl(Function2<R, A, R > f,  R init, Iterable<A> it) {
+    public static <A, R> R foldl(Function2<? super R, ? super  A,  ? extends R > f,  R init, Iterable<A> it) {
         for (A el : it){
             init = f.apply(init, el);
         }
         return init;
     }
 
-    private static <A, R> R foldr_use(Function2<A, R, R > f,  R init, Iterator<A> iter) {
+    private static <A, R> R foldr_use(Function2<? super A, ? super R,  ? extends R >  f,  R init, Iterator<A> iter) {
         if (iter.hasNext()){
             A el = iter.next();
             return f.apply(el, foldr_use(f, init, iter));
@@ -63,7 +60,7 @@ public class Collections {
         else return init;
     }
 
-    public static <A, R> R foldr(Function2<A, R, R > f,  R init, Iterable<A> it) {
+    public static <A, R> R foldr(Function2<? super A, ? super R, ? extends R > f,  R init, Iterable<A> it) {
         return foldr_use(f, init, it.iterator());
     }
 }
